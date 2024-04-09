4c250ff4ad89f721beab231e51478c1a1614d548
230656 <100227543+230656@users.noreply.github.com>
2024-04-09 22:55:56 +0200

    m

--- exercises/Min_Max.py
+++ exercises/Min_Max.py
@@ -10,7 +10,12 @@ def getSmallest(numbers):
     return smallest
 
 assert getSmallest([1, 2, 3]) == 1
+print(getSmallest([1, 2, 3]))
 assert getSmallest([3, 2, 1]) == 1
+print(getSmallest([3, 2, 1]))
 assert getSmallest([28, 25, 42, 2, 28]) == 2
+print(getSmallest([28, 25, 42, 2, 28]))
 assert getSmallest([1]) == 1
+print(getSmallest([1]))
 assert getSmallest([]) == None
+print(getSmallest([]))
